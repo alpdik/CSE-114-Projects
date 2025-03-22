d5125410be769c0d3f50b4f2b721404de2112907
Alp Dikmen <alp.dikmen@std.yeditepe.edu.tr>
2025-03-22 15:58:36 +0300

    labwork of march 21

--- alp_dikmen.c
+++ alp_dikmen.c
@@ -41,7 +41,7 @@ int ave_of_cubes_of_nums_between(int num1, int num2)
         sum+=num2*num2*num2;
         num2-=1;
     }
-    return sum;
+    return sum/(num2-num1-1);
 }
 
 int gcd(int num1, int num2)
@@ -98,28 +98,108 @@ int is_perfect_number(int number)
 
 int sum_of_digits(int number)
 {
-    int sum,pot;
-    
+    int sum,temp;
     sum=0;
-    pot=100000;
-    
+
     while(number>0)
-    {   
-        number/=pot;
-        sum+=number;
-        pot/=10;
+    {
+        temp=number%10;
+        sum+=temp;
+        number=(number-temp)/10;
     }
-    
+
     return sum;
+}
+
+int is_prime(int number)
+{
+    int temp;
+    temp=number-1;
+
+    if (number==1)
+    {
+        return 0;
+    }
+
+    while (temp>1)
+    {
+        if ((number%temp)==0)
+        {
+            return 0;
+        }
+        temp--;
+    }
+
+    return 1;
 }
 
 
 int main()
 {
-    int x;
-    x=sum_of_digits(123);
-    
-    printf("%d",x);
+    char uchc;
+    printf("Please select a operation:\n");
+    scanf("%c",&uchc);
+
+    if (uchc=='e')
+    {
+        int base,power;
+        printf("Enter the base: ");
+        scanf("%d",&base);
+        printf("Enter the power: ");
+        scanf("%d",&power);
+        printf("%d\n",exponent(base,power));
+    }
+    else if (uchc=='f')
+    {
+        int number;
+        printf("Enter the number: ");
+        scanf("%d",&number);
+        printf("%d\n",factorial_even(number));
+    }
+    else if (uchc=='s')
+    {
+        int num1,num2;
+        printf("Enter the first number: ");
+        scanf("%d",&num1);
+        printf("Enter the second number: ");
+        scanf("%d",&num2);
+        printf("%d\n",ave_of_cubes_of_nums_between(num1,num2));
+    }
+    else if (uchc=='g')
+    {
+        int num1,num2;
+        printf("Enter the first number: ");
+        scanf("%d",&num1);
+        printf("Enter the second number: ");
+        scanf("%d",&num2);
+        printf("%d\n",gcd(num1,num2));
+    }
+    else if (uchc=='p')
+    {
+        int number;
+        printf("Enter the number: ");
+        scanf("%d",&number);
+        printf("%d\n",is_perfect_number(number));
+    }
+    else if (uchc=='t')
+    {
+        int number;
+        printf("Enter the number: ");
+        scanf("%d",&number);
+        printf("%d\n",sum_of_digits(number));
+    }
+    else if (uchc=='r')
+    {
+        int number;
+        printf("Enter the number: ");
+        scanf("%d",&number);
+        printf("%d\n",is_prime(number));
+    }
+    else
+    {
+        printf("INVALID INPUT\n");
+    }
+
 
     return 0;
 }
