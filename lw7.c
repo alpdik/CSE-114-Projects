533eb6bb72a899f1f02e50448e5beafcd17c87cd
Alp Dikmen <alp.dikmen@std.yeditepe.edu.tr>
2025-05-17 13:36:35 +0300

    lw 7

--- lw7.c
+++ lw7.c
@@ -9,12 +9,12 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <stdio.h>
 #define SIZE 5
 
-void strConcat(char *dest, char* str1, char* str2)
+void strConcat(char *dest,char* str1, char* str2)
 {
     int x=0;
     
     
-    for(x; *(str1+x)!='\0'; x++)
+    for(; *(str1+x)!='\0'; x++)
     {
        *(dest+x)=*(str1+x);
     }
@@ -22,10 +22,12 @@ void strConcat(char *dest, char* str1, char* str2)
     *(dest+x)=' ';
     x++;
     
-    for(int y=0; *(str2+y)!=0; y++,x++)
+    for(int y=0; *(str2+y)!='\0'; y++,x++)
     {
        *(dest+x)=*(str2+y);
     }
+
+    *(dest + x) = '\0';
 }
 
 int isPalindrome( char *str ){
@@ -50,67 +52,98 @@ int isPalindrome( char *str ){
     return 1;
 }
 
-void  removeSpaces(char* str)
-{
-    int x=0;
-    char *dest;
-    
-    for(x; *(str+x)!='\0'; x++)
-    {
-       if(*(str+x)!=' ')
-       {
-        *(dest+x)=*(str+x);
-       }
-    }
-    
-    str=dest;
-}
+void removeSpaces(char *str) {
+    int x = 0, y = 0;
 
-int compareIgnoreCase(char* str1, char* str2)
-{
-    for(int x=0;*(str1+x)=='\0';x++)
+    // Remove spaces
+    for (;*(str + x)!='\0'; x++)
     {
-        if(*(str1+x)!=*(str2+x))
+        if (*(str + x) != ' ')
         {
-            if(*(str1+x)+32!=*(str2+x))
-            {
-                if(*(str1+x)!=*str2+32)
-                {
-                    if (str1>str2)
-                    {
-                        return 1;
-                
-                    }
-                    return -1;
-                }
-            }
+            *(str + y) = *(str + x);
+            y++;
         }
     }
-    
-    return 0;
+
+    *(str + y) = '\0';
+}
+
+int compareIgnoreCase(char* str1, char* str2,int start)
+{
+    if (str1[start]=='\0'&&str2[start]=='\0')
+    {
+        return 0;
+    }
+
+
+
+    if (str1[start] == str2[start])
+    {
+        compareIgnoreCase(str1,str2,start+1);
+    }
+    else if (str1[start]+32 == str2[start])
+    {
+        compareIgnoreCase(str1,str2,start+1);
+    }
+    else if (str1[start] == str2[start]+32)
+    {
+        compareIgnoreCase(str1,str2,start+1);
+    }
+    else
+    {
+        if (str1[start] > str2[start])
+        {
+            return 1;
+        }
+        return -1;
+    }
+
 }
 
 
-char *concatArray(char* array[], int size)
+char *concatArray(char* array[])
 {
-    
+    char *endstring;
+
+    for (int i=0,k=0;array[i]!='\0';i++)
+    {
+        for (int j=0;array[i][j]!='\0';j++,k++)
+        {
+            endstring[k]=array[i][j];
+        }
+    }
+    return endstring;
 }
 
 void  sortArray(char* array[], int size)
 {
     char *temp;
-    
+
     for( int i2 = 0; i2 < size-1; i2++ ){
         for( int i = 0; i < size-1; i++ ){
-            if(array[i+1] < array[i])
+
+            int x=0;
+
+            if (array[i+1][x]==array[i][x])
             {
+                for (;array[i+1][x]=='\0' && array[i+1][x]=='\0';x++)
+                {
+                    if(array[i+1][x]<array[i][x])
+                    {
+                        return;
+                    }
+                }
+            }
+            if(array[i+1][x]<array[i][x])
+            {
+
                 temp=array[i+1];
                 array[i+1]=array[i];
                 array[i]=temp;
-            
+
             }
 
-        
+
         }
     }
 }
@@ -118,23 +151,58 @@ void  sortArray(char* array[], int size)
 int main()
 {
     char *A[SIZE]={"madam", "hello", "racecar", "world", "civic"};
-    
+
+
+    char nonpali[200]="";
     printf("Palindromes: ");
     for(int x=0;x<SIZE;x++)
     {
-        
+
         if(isPalindrome(A[x])==1)
         {   
             
             for(int y=0; A[x][y]!='\0' ;y++)
-            {  
+            {
                 printf("%c",A[x][y]);
             }
             
             printf(" ");
         }
+
+        if(isPalindrome(A[x])==0)
+        {
+            strConcat(nonpali, nonpali, A[x]);
+        }
+
     }
     printf("\n");
+
+
+    printf("Concatenated Non-Palindromes: %s\n",nonpali);
+
+
+    char samplestring[6]="a b c";
+    removeSpaces(samplestring);
+    printf("Space-Free String: %s\n",samplestring);
+
+
+    printf("Case-Insensitive Comparison: %s vs %s -> Equal (%d)\n","Hello","hello",compareIgnoreCase("Hello","hello",0));
+
+    sortArray(A, SIZE);
+    printf("Sorted Array: ");
+    for(int x=0;x<SIZE;x++)
+    {
+        printf("%s ",A[x]);
+    }
+    printf("\n");
+
+    char concatsorted[200]="";
+    for(int x=0;x<SIZE;x++)
+    {
+        strConcat(concatsorted, concatsorted, A[x]);
+    }
+    printf("Concatenated Sorted Array: %s\n",concatsorted);
+
 
     return 0;
 }
